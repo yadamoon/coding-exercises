a0c2cb7f3ac84edaa82e24ce3195cf74cd41e0cf
Yared <yared241993@gmail.com>
2023-12-07 18:18:50 +0300

    It works by initializing a variable sum to zero, and then iterating over each element of the array using a for loop. For each element, it checks if the element is even by checking if arr[i] % 2 === 0. If the element is even, it adds it to the sum variable using the += operator. Finally, it returns the sum variable.

--- Sum of Even Numbers/index.js
+++ Sum of Even Numbers/index.js
@@ -3,8 +3,9 @@ const sumOfEvenNumbers = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sum += arr[i];
+      console.log(sum);
     }
-    return console.log(sum);
   }
+  return console.log(sum);
 };
-sumOfEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+sumOfEvenNumbers([1, 2, 3, 4, 5, 6]);
