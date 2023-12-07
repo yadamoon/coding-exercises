f6b75aa1967989b8c11c81f72abdd7ab3cb50596
Yared <yared241993@gmail.com>
2023-12-07 18:19:57 +0300

    updated the message part

--- Prime Number Checker/index.js
+++ Prime Number Checker/index.js
@@ -4,9 +4,13 @@ const isPrime = (num) => {
   }
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
+      console.log("is not Prime Number ");
       return false;
     }
   }
-
+  console.log("yes the number are Prime Number");
   return true;
 };
+
+isPrime(7);
+isPrime(10);
