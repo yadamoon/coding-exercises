0dfa78a9a96bc0b802c0a28ea3bf7f9524cd8542
Yared <yared241993@gmail.com>
2023-12-07 18:07:20 +0300

    Then, it uses a for loop to iterate from 2 up to the square root of the num. For each iteration, it checks if the num is divisible evenly by the current iteration value. If it is, then the num is not prime and the function returns false

--- Prime Number Checker/index.js
+++ Prime Number Checker/index.js
@@ -2,4 +2,11 @@ const isPrime = (num) => {
   if (num <= 1) {
     return console.log(`${num} is less than 1 or equal one `);
   }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
