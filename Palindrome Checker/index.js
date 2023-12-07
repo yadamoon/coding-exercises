50b7251d3e144892bbcac94e893eddaec7a147cc
Yared <yared241993@gmail.com>
2023-12-07 17:01:16 +0300

    then compire the parameter and local varible

--- Palindrome Checker/index.js
+++ Palindrome Checker/index.js
@@ -1,5 +1,10 @@
 const palindromeCheker = (str) => {
   let local = str.split("").reverse().join("");
+  if (local === str) {
+    console.log("palindrome");
+  } else {
+    console.log("not palindrome");
+  }
 };
 
 palindromeCheker("1211");
